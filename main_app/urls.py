74d61bab331ba912aef4a5f58495876413e29f52
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-24 13:45:30 -0400

    fixed last commit

--- main_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-# Add the include function to the import
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # In this case '' represents the root route
-    path('home/', include('main_app.urls')),
-]
