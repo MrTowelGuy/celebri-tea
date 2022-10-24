c71ad0f196c5b1364d8a44b4afb38877cadacb16
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-24 19:44:34 -0400

    tea id_bug getting worked

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('tea/', views.tea_index, name='index'),
+  path('tea/<int:tea_id>/', views.tea_detail, name='detail'),
+  path('tea/create/', views.TeaCreate.as_view(), name='tea_create'),
+  path('tea/<int:pk>/update/', views.TeaUpdate.as_view(), name='tea_update'),
+  path('tea/<int:pk>/delete/', views.TeaDelete.as_view(), name='tea_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Tea
 
 # Define the home view
 def home(request):
@@ -13,20 +13,25 @@ def about(request):
 
 # Add new view
 def tea_index(request):
+  tea = Tea.objects.all()
   return render(request, 'tea/index.html', { 'tea': tea })
 
+def tea_detail(request, tea_id):
+  tea = Tea.objects.all(id=tea_id)
+  return render(request, 'tea/detail.html', { 'tea': tea })
 
 
-# Add the Cat class & list and view function below the imports
-class Tea:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, title, type, description, witnesses):
-    self.title = title
-    self.type = type
-    self.description = description
-    self.witnesses = witnesses
+class TeaCreate(CreateView):
+  model = Tea
+  fields = ['title','type','description','witnesses']
+  success_url = '/tea/'
 
-tea = [
-  Tea('Elon public intoxication', 'embarassing', 'A disoriented Elon Musk was sited in Helen Ga tripping on peyote. Eye witnesses have reported that he was in nothing but an adventure time bathrobe and a batman mask. He was apparently seen trying to administer cpr to a dead goldfish and crying his stupid little eyes out like a loser.', 7),
-  Tea('Mickey Mouse Adultry House', 'adultry', 'Minnie, walking into her house with the camera crew from cheaters, found Mickey naked in the pantry with Courtney Love. Sources say that Mickey was somehow responsible for Courtney Love thinking that it was okay for her to make music.',10),
-  Tea('Living inCyde', 'Unexpected', 'Leaked emails spill content showing that the frontman of Living in Fear, Curtis, had been emailing the members of crunk-core group BrokeNcyde to express his passion and love for their band. When said emails leaked BC-13 came on record to say that they wanted to set up a tour with them and Living in Fear. Curtis has since denied all allegations of writing such an email, saying that "BrokeNcyde is straight dog water ass clown music". Recent chilling updates had eye witnesses seeing Curtis listening to Bree Bree on their spotify friend activity, Curtis forgetting to put his spotify on a private listening session.', 4569)
-]
+
+class TeaUpdate(UpdateView):
+  model = Tea
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['type', 'description', 'witnesses']
+
+class TeaDelete(DeleteView):
+  model = Tea
+  success_url = '/tea/'
